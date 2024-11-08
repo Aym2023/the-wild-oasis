622217f90395f5c748290924bc23a81a05657b2e
ayman saleh <ayman.saleh000@gmail.com>
2024-11-08 23:45:10 +0200

    intial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const H1 = styled.h1`font-size = 15 px,
+font-waight = 600, 
+background : purble
+`;
+
+
+function App() {
+  return (
+    <H1>
+      Welcome World
+    </H1>
+  )
+}
+
+export default App;
