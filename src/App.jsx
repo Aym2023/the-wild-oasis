5f8cf262c5ae89eb17f7cd3bfb29593bede1df96
ayman saleh <ayman.saleh000@gmail.com>
2024-11-09 22:30:59 +0200

    modyfied

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,34 @@
 import styled from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Button from './ui/Button';
+import Input from './ui/Input';
 
-const H1 = styled.h1`font-size = 15 px,
-font-waight = 600, 
-background : purble
+const H1 = styled.h1`
+font-size: 30px;
+font-weight: 600;
+background-color: yellow;
 `;
 
 
+const StyledApp = styled.main`
+background-color: orangered;
+padding: 20px;
+`;
+
 function App() {
   return (
-    <H1>
-      Welcome World
-    </H1>
-  )
+    <>
+    <GlobalStyles />
+    <StyledApp>
+    <H1>Welcome World</H1>
+    <Button onClick={() => alert('Check In')}>Check In</Button>
+    <Button onClick={() => alert('Check Out')}>Check Out</Button>
+
+    <Input type='number' placeholder='number of guests'/>
+    <Input type='number' placeholder='number of guests'/>
+    </StyledApp>
+    </>
+  );
 }
 
 export default App;
