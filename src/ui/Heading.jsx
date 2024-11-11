95571e61d8833d06bd4c740e8ddd6e5fb9c1612f
ayman saleh <ayman.saleh000@gmail.com>
2024-11-11 11:47:21 +0200

    modyfied

--- src/ui/Heading.jsx
+++ src/ui/Heading.jsx
@@ -5,21 +5,18 @@ ${(props) => props.as === 'h1' &&
     css`
 font-size: 4rem;
 font-weight: 600;
-background-color: yellow;
 `}
 
  ${(props) => props.as === 'h2' &&
  css`
  font-size: 3rem;
 font-weight: 600;
-background-color: yellow;
 `}
 
 ${(props) => props.as === 'h3' &&
   css`
 font-size: 2rem;
 font-weight: 600;
-background-color: yellow;
 `}   
 
 line-height: 1.4;
