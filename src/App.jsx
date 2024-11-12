b5b3cb99aee4f05019fd1bb762f0c116dbffdf6b
ayman saleh <ayman.saleh000@gmail.com>
2024-11-13 00:49:44 +0200

    modyfied

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom"
+import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { QueryClient, QueryClientProvider,  } from "@tanstack/react-query";
+import { ReactQueryDevtools }  from "@tanstack/react-query-devtools";
+
 import GlobalStyles from './styles/GlobalStyles';
 
 import Dashboard from './Pages/Dashboard';
@@ -11,10 +14,19 @@ import Login from './Pages/Login';
 import PageNotFound from './Pages/PageNotFound';
 import AppLayout from "./ui/AppLayout";
 
+const queryClient = new QueryClient({
+  defaultOptions:{
+   queries:{
+    staleTime: 60 * 1000,
+   }, 
+  },
+});
 
 function App() {
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
+    <ReactQueryDevtools intialIsOpen={false} />
+
     <GlobalStyles/>
       <BrowserRouter>
       <Routes>
@@ -34,7 +46,7 @@ function App() {
 
       </Routes>
       </BrowserRouter>
-      </>
+      </QueryClientProvider>
   )
 }
 
